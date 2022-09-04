10b577042cbd9b8f5c43c73f05e8fb80a39d01c5
NickGorjestani <nickg2794@hotmail.com>
2022-09-04 13:18:07 -0500

    Week 3 homework

--- Homework/Week3Homework.sql
+++ Homework/Week3Homework.sql
@@ -13,6 +13,19 @@ where c.StoreID is not null
 
 -- 2. Rank products by total number of units sold to show which products sell best. Display the name, the total units sold, and the ranking.
 
-
+select rank() over (order by x.TotalOrders desc) Rank, *
+from (select distinct
+    sod.ProductID,
+    p.Name,
+    count(*) over ( partition by sod.ProductID ) TotalOrders
+from Sales.SalesOrderHeader soh
+    join Sales.SalesOrderDetail sod on soh.SalesOrderID = sod.SalesOrderID
+    join Production.Product p on sod.ProductID = p.ProductID) x
 
 -- 3. Display each employee’s time-off hours as a separate row – one row for vacation hours, another for sick.
+
+select * from (select p.FirstName + ' ' + p.LastName Name, e.VacationHours, e.SickLeaveHours
+from HumanResources.Employee e
+    join Person.Person p
+    on e.BusinessEntityID = p.BusinessEntityID) pvt
+pivot (  )
