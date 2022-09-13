b50643c36efbb19015cbb4b24c735d1b832fe297
NickGorjestani <nickg2794@hotmail.com>
2022-09-12 21:51:10 -0500

    Week 4 homework

---
+++ Homework/Week4Homework.sql
@@ -0,0 +1,28 @@
+begin tran
+
+update Production.Product
+set StandardCost = (p.StandardCost * 1.1)
+from Production.Product p
+    join Production.ProductSubcategory ps
+    on p.ProductSubcategoryID = ps.ProductSubcategoryID
+where ps.Name = 'Tires and Tubes'
+
+update Production.ProductCostHistory
+set EndDate = GETDATE()
+where ProductID in (select p.ProductID
+from Production.Product p
+    join Production.ProductSubcategory ps
+    on p.ProductSubcategoryID = ps.ProductSubcategoryID
+where ps.Name = 'Tires and Tubes') and EndDate is null
+
+insert into Production.ProductCostHistory
+select pch.ProductID, GETDATE(), null, pch.cost, GETDATE()
+from (select pch.*, p.StandardCost as cost from Production.ProductCostHistory pch
+    join Production.Product p on P.ProductID = pch.ProductID
+    where pch.ProductID in (select p.ProductID
+    from Production.Product p
+        join Production.ProductSubcategory ps
+        on p.ProductSubcategoryID = ps.ProductSubcategoryID
+    where ps.Name = 'Tires and Tubes')) pch
+
+commit
