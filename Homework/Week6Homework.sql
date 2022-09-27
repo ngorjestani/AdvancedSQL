24f17e7b8ae6adedca67f5d24fa5b9a164a780de
NickGorjestani <nickg2794@hotmail.com>
2022-09-26 22:26:15 -0500

    Week 6 homework

--- Homework/Week6Homework.sql
+++ Homework/Week6Homework.sql
@@ -2,16 +2,65 @@ declare @FirstDate date
 select @FirstDate = '7/4/2018'
 
 declare @LastDate date
-select @LastDate = '7/5/2018'
+select @LastDate = '12/31/2020'
 
+declare @CostAtTimeOfOrder table(WorkOrderID int, StandardCost money)
+
+-- get cost at time of order
+insert into @CostAtTimeOfOrder
 select wo.WorkOrderID,
-       p.Name  as ProductName,
-       sr.Name as ScrapReason,
-       wo.ScrappedQty
+       pch.StandardCost
 from Production.WorkOrder wo
-         join Production.ScrapReason sr
-              on wo.ScrapReasonID = sr.ScrapReasonID
          join Production.Product p
-              on p.ProductID = wo.ProductID
+              on wo.ProductID = p.ProductID
+         left join Production.ProductCostHistory pch
+              on p.ProductID = pch.ProductID
 where wo.StartDate between @FirstDate and @LastDate
   and wo.ScrapReasonID is not null
+  and pch.StartDate < wo.StartDate
+  and (pch.EndDate is null or pch.EndDate > wo.StartDate)
+
+-- get calculated work order info
+select wo.WorkOrderID,
+       p.Name ProductName,
+       sr.Name ScrapReason,
+       wo.ScrappedQty,
+       catoo.StandardCost,
+       wo.ScrappedQty * catoo.StandardCost ScrappedMetalCost,
+       sum(wor.ActualResourceHrs) * l.CostRate / wo.OrderQty,
+       l.CostRate,
+       wo.OrderQty
+from Production.WorkOrder wo
+         join Production.WorkOrderRouting wor
+              on wo.WorkOrderID = wor.WorkOrderID
+         join Production.Location l
+              on wor.LocationID = l.LocationID
+         join Production.Product p
+              on wo.ProductID = p.ProductID
+         join Production.ScrapReason sr
+              on wo.ScrapReasonID = sr.ScrapReasonID
+         left join @CostAtTimeOfOrder catoo
+              on wo.WorkOrderID = catoo.WorkOrderID
+where wo.StartDate between @FirstDate and @LastDate
+  and wo.ScrapReasonID is not null
+group by wo.WorkOrderID,
+         p.Name,
+         sr.Name,
+         wo.ScrappedQty,
+         catoo.StandardCost,
+         wo.ScrappedQty * catoo.StandardCost,
+         l.CostRate,
+         wo.OrderQty
+
+/*
+WorkOrderID: Production.WorkOrder
+ProductName: Production.Product
+ScrapReason: Production.ScrapReason
+ScrappedQty: Production.WorkOrder
+StandardCost: Production.ProductCostHistory on date of Production.WorkOrder.StartDate
+ScrappedMetalCost: Qty * StandardCost
+LaborCost: Production.WorkOrderRouting.ActualResourceHrs * Production.Location.CostRate / Production.WorkOrder.OrderQty
+*/
+
+select l.LocationID, l.CostRate
+from Production.Location l
